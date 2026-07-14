b5af05cc129e59c119769f4953b915897a990011
Simone Ghilotti <simoneghilotti@gmail.com>
2026-07-14 11:52:18 +0100

    searchbar complete

--- src/Search.tsx
+++ src/Search.tsx
@@ -12,25 +12,28 @@ function Search() {
 
   //States
   const[city, setCity] = useState<string>("");
+  const[cityData, setCityData] = useState<City[]>([]);
+  const[togglePreview, setTogglePreview] = useState<boolean>(false);
 
   //Hooks
     useEffect(() => {
         if(city) {
         const cityData : City [] = ukCityData.filter((c: City) => c?.id.toLowerCase().includes(city));
+        setCityData(cityData);
         }
     }, [city]);
 
-    //TODO: set preview of cities that match the search query, and on click set the city to the one clicked on
+    //TODO: fix right header white gap.
 
-  
+  //Handlers
   function searchCity(city: string): void {
     if((/^[a-zA-Z\s]+$/).test(city)) {
         setCity(city.toLowerCase());
     }
   }
-
   
   return (
+    <>
     <div className="bg-blue-500 w-full p-4">
         <div className="grid grid-cols-12 max-w-xl mx-auto">
             <div className="col-span-9 bg-amber-500 border-8 rounded-full border-blue-500">
@@ -38,7 +41,7 @@ function Search() {
                   className="w-full p-2 border-2 rounded-full border-black bg-transparent text-white text-sm md:text-1xl lg:text-1xl placeholder-white placeholder-opacity-100 outline-none text-center" 
                   type="search" 
                   placeholder="Search for a city..." 
-                  onChange={(e) => searchCity(e.target.value)}
+                  onChange={(e) => searchCity(e.target.value)} onFocus={() => setTogglePreview(true)} onBlur={() => setTogglePreview(false)}
                 />
             </div>
             <div className="col-span-3 bg-amber-500 border-8 rounded-full border-blue-500">
@@ -47,7 +50,26 @@ function Search() {
                 </button>
             </div>
         </div>
+        {togglePreview && (
+          <div className="bg-blue-500 w-full" onMouseDown={(e) => e.preventDefault()}>
+            <div className="grid grid-cols-12 gap-8">
+              <div className= "col-span-3 *:text-white text-sm md:text-1xl lg:text-2xl p-6 rounded"><h1>Search weather in...</h1></div>
+              <div className= "col-span-9 text-white text-sm  p-6 rounded">
+                {(city.length > 0) && (
+                  <ul>
+                    {cityData.map((c: City) => (
+                      <li key={c?.id} className="cursor-pointer transition-colors hover:bg-blue-700 duration-300 p-2 rounded" >
+                        <a className="block w-full h-full" href={`/view/${c?.id}`}>{c?.name}</a>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            </div>
+          </div>
+        )}
     </div>
+  </>
   )
 }
 
